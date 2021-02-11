8816f0dee90528de838ca2c2698645cb82a8f518
phxtho <phathopukwana@gmail.com>
2021-02-11 22:45:35 +0200

    implement hittagble list

--- src/structures/hittable.rs
+++ src/structures/hittable.rs
@@ -1,13 +1,41 @@
 use crate::structures::vec3::Vec3;
 use crate::structures::ray::Ray;
 
+#[derive(Clone,Copy)]
 pub struct HitRecord {
     pub point : Vec3,
     pub normal: Vec3,
-    pub t: f64
+    pub t: f64,
+    pub front_face: bool
 }
 
+impl HitRecord {
+    pub fn set_face_normal(&mut self, r : &Ray, outward_nomral : &Vec3) {
+        self.front_face = r.direction.dot(outward_nomral) < 0.0;
+        self.normal = if self.front_face {*outward_nomral} else {-*outward_nomral};
+    }
+}
 
 pub trait Hittable {
     fn hit (&self, r :&Ray, t_min : f64, t_max :f64, rec : &mut HitRecord ) -> bool;
+}
+
+pub struct HittableList {
+    pub objects : Vec<Box<dyn Hittable>>
+}
+
+impl Hittable for HittableList {
+    fn hit (&self, r :&Ray, t_min : f64, t_max :f64, rec : &mut HitRecord ) -> bool{
+        let mut hit_anything = false;
+        let mut closest_so_far = t_max;
+
+        for object in self.objects.iter() {
+            if object.hit(r,t_min, closest_so_far, rec) {
+                hit_anything = true;
+                closest_so_far = rec.t;       
+            }
+        }
+
+        return hit_anything
+    } 
 }
